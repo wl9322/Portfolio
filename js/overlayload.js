64eb1e8212e229af4d5064204c29fe0cc5f02216
wl9322 <wl9322@gmail.com>
2022-06-14 12:40:28 +0900

    ing

--- js/overlayload.js
+++ js/overlayload.js
@@ -1,13 +1,12 @@
 function myload(url) {
 	$(".project-overlay").load(url);
-	console.log("hi");	
 	$(".project-overlay").css("display", "block");
 }
 
 
 function closeOverlay() {
 	let curURL = $(window.location)[0].href;
-	let lastIndex = curURL.lastIndexOf("#") - 1;
+	let lastIndex = curURL.lastIndexOf("#");
 	let url = curURL.substring(0, lastIndex);
 
 	window.history.pushState(null, null, url);
@@ -15,9 +14,7 @@ function closeOverlay() {
 }
 
 $("a[name='overlay-workTest']").click(function(event) {
-	console.log(event);
 	event.preventDefault();
-	console.log("hello");
 	let url = "project/project-overlay.html";
 	let curURL = $(window.location)[0].href;
 	let lastIndex = curURL.lastIndexOf("/");
